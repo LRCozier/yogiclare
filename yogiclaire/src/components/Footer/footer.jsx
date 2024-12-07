9507f4041d136591011feeaa8cf207c5ffc58001
LRCozier <lrcozier@gmail.com>
2024-12-07 10:17:52 +0000

    created footer component

---
+++ yogiclaire/src/components/Footer/footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+
+  return(
+    <>
+    <div>
+      <footer>
+        <p>&copy; 2025 Yogi Claire. All rights reserved.</p>
+          <div class="social-links">
+              <a href="#">Facebook</a>
+              <a href="#">Instagram</a>
+          </div>
+      </footer>
+    </div>
+    </>
+  )
+}
+
+export default Footer;

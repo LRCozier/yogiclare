22f2e34eb98101706b103af83adbf4cf0f6f3512
LRCozier <lrcozier@gmail.com>
2024-12-07 10:17:43 +0000

    created header components

---
+++ yogiclaire/src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+
+  return(
+    <>
+    <Header>
+      <div class="logo">Yogi Claire</div>
+    </Header></>
+  )
+}
+
+export default Header;

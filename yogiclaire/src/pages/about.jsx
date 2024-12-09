a6b0ce7e1d4879e5fbf0811fdfbfef1edaa06b32
LRCozier <lrcozier@gmail.com>
2024-12-09 08:25:28 +0000

    added image to section

--- yogiclaire/src/pages/about.jsx
+++ yogiclaire/src/pages/about.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import yogaheadshot from "./assets/images/yogaheatshot.jpeg";
 
 const About = () => {
 
@@ -8,6 +9,7 @@ const About = () => {
     <div className="container">
       <section className="yogiclaire-section">
         <h2>About Me</h2>
+        <img src={yogaheadshot} alt="yogi claire yoga headshot" />
         <p>Yoga is more than just physical postures; it's a holistic practice that connects body, mind, and spirit, 
         bringing balance and harmony in a chaotic world. As a teacher, I believe yoga is a transformative tool, 
         inviting us to breathe deeply, move mindfully, and explore our inner selves with compassion. It fosters 

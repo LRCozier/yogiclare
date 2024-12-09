bdf353769c05c992311957989d15d3f753f9c04c
LRCozier <lrcozier@gmail.com>
2024-12-09 08:27:30 +0000

    created page and added contact form component

---
+++ yogiclaire/src/pages/contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ContactForm from "../components/Contact Form/contactform";
+
+const Contact = () => {
+
+  return (
+    <>
+    <h2>Contact</h2>
+    <p></p>
+    <ContactForm/>
+    </>
+  )
+}
+
+export default Contact;

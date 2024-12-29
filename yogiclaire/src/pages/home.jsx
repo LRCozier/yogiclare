87f6cee200ea9963ee0522418e21e19c7bc86b07
LRCozier <lrcozier@gmail.com>
2024-12-29 19:19:45 +0000

    added buttons

--- yogiclaire/src/pages/home.jsx
+++ yogiclaire/src/pages/home.jsx
@@ -8,6 +8,11 @@ const Home = () => {
       <section className="yogiclaire-section">
         <h1>Yogi Claire</h1>
         <p>Unite Your Body, Spirit & Mind</p>
+        <div className="yogiclaire-home-buttoncontainer">
+          <button>Explore Classes</button>
+          <button>Yoga In Education</button>
+          <button>Contact Me</button>
+        </div>
       </section>
     </div>
     </>
